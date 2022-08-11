f6eb0dad49b253ce557e5f257ad17722de2acf5e
Richard Torres <rtorresbarriga@gmail.com>
2022-08-10 20:46:02 -0400

    Hola mundo

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,15 +1,15 @@
 # 1. TAREA: imprime "Hola, mundo"
 print( "Hola, mundo" )
 # 2. imprime "Hola, Noelle" con el nombre en una variable
-name = "Noelle"
+name = "Richard"
 print( "Hola", name )	# con una coma
 print( "Hola " + name )	# con un +
 # 3. imprimir "Hola 42!" con el número en una variable
-number = 42
+number = 10
 print( "Hola", number )	# con una coma
-print( "Hola" + 42 )	# con una +	-- este debería arrojar un error!
+print( "Hola " + str(number))	# con una +	-- este debería arrojar un error!
 # 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
-fave_food1 = "sushi"
+fave_food1 = "hamburguesa"
 fave_food2 = "pizza"
 print( "Amo comer {} y {}".format(fave_food1,fave_food2) ) # con .format()
 print(f'Amo comer {fave_food1} y {fave_food2}' ) # con una cadena f

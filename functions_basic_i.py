24cb60dcbc6b50191c137f3850ecc12e3b298f2a
Richard Torres <rtorresbarriga@gmail.com>
2022-08-12 01:30:48 -0400

    Nuevo

--- functions_basic_i.py
+++ functions_basic_i.py
@@ -1,46 +1,46 @@
 #1
 def number_of_food_groups():
     return 5
-print(number_of_food_groups())
+print(number_of_food_groups()) #5
 
 
 #2
 def number_of_military_branches():
     return 5
-print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches()) #error
 
 
 #3
 def number_of_books_on_hold():
     return 5
     return 10
-print(number_of_books_on_hold())
+print(number_of_books_on_hold()) #5
 
 
 #4
 def number_of_fingers():
     return 5
     print(10)
-print(number_of_fingers())
+print(number_of_fingers()) #5
 
 
 #5
 def number_of_great_lakes():
-    print(5)
+    print(5) 
 x = number_of_great_lakes()
-print(x)
+print(x) #no definida
 
 
 #6
 def add(b,c):
     print(b+c)
-print(add(1,2) + add(2,3))
+print(add(1,2) + add(2,3)) #error
 
 
 #7
 def concatenate(b,c):
     return str(b)+str(c)
-print(concatenate(2,5))
+print(concatenate(2,5)) #25
 
 
 #8
@@ -52,7 +52,7 @@ def number_of_oceans_or_fingers_or_continents():
     else:
         return 10
     return 7
-print(number_of_oceans_or_fingers_or_continents())
+print(number_of_oceans_or_fingers_or_continents()) #10
 
 
 #9
@@ -62,51 +62,51 @@ def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
     else:
         return 14
     return 3
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3)) #7
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3)) #14
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3)) #21
 
 
 #10
 def addition(b,c):
     return b+c
     return 10
-print(addition(3,5))
+print(addition(3,5)) #8
 
 
 #11
 b = 500
-print(b)
+print(b) # 500
 def foobar():
     b = 300
     print(b)
-print(b)
-foobar()
-print(b)
+print(b) # 500
+foobar() # 300
+print(b) # 500
 
 
 #12
 b = 500
-print(b)
+print(b) # 500
 def foobar():
     b = 300
     print(b)
     return b
-print(b)
-foobar()
-print(b)
+print(b) # 500
+foobar() # 300
+print(b) # 500
 
 
 #13
 b = 500
-print(b)
+print(b) # 500
 def foobar():
     b = 300
     print(b)
     return b
-print(b)
-b=foobar()
-print(b)
+print(b) # 500
+b=foobar() # 300
+print(b) # 300
 
 
 #14
@@ -116,7 +116,7 @@ def foo():
     print(2)
 def bar():
     print(3)
-foo()
+foo() # 1,3,2
 
 
 #15
@@ -129,4 +129,4 @@ def bar():
     print(3)
     return 5
 y = foo()
-print(y)
+print(y) # 1,3,5,10

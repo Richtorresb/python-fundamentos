a5886ad7e67633b4a04db87f76bceae7cc2d00c3
Richard Torres <rtorresbarriga@gmail.com>
2022-08-17 12:19:09 -0400

    Asociacion entre clases CORE

--- asociacion_entre_clases.py
+++ asociacion_entre_clases.py
@@ -29,38 +29,51 @@ class Usuario:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.cuenta = CuentaBancaria(0.02, balance=0)	
+        self.cuenta = {}
 
-    def hacer_deposito(self,amount):
-        self.cuenta.deposito(amount)
+    def crear_cuenta(self,nombre_cuenta,tasa_interes):
+        self.cuenta[nombre_cuenta] = CuentaBancaria(tasa_interes)
+        print(f'Cuenta nueva: {nombre_cuenta}, Tasa de interes: {tasa_interes}')
         return self
 
-    def retiro(self, amount):
-        self.cuenta.retiro(amount)
+    def hacer_deposito(self,nombre_cuenta,amount):
+        self.cuenta[nombre_cuenta].deposito(amount)
+        print(f'Deposito por : {amount}, en Cuenta :{nombre_cuenta}')
+        return self
+
+    def retiro(self,nombre_cuenta, amount):
+        self.cuenta[nombre_cuenta].retiro(amount)
+        print(f'Retiro por: {amount}, Cuenta: {nombre_cuenta}')
         return self
     
-    def transferencia(self,user,amount):
-        self.cuenta.retiro(amount)
-        if self.cuenta.balance >= 0:
-            user.hacer_deposito(amount)
+    def transferencia_interna(self,nombre_cuenta,nombre_a_transferir,amount):
+        self.cuenta[nombre_cuenta].retiro(amount)
+        if self.cuenta[nombre_cuenta].balance >= 0:
+            self.cuenta[nombre_a_transferir].deposito(amount)
+            print(f'Transferencia realizada a {nombre_a_transferir} por {amount}')
 
-    
-    def mostrar_balance_usuario(self):
+    def mostrar_balance_usuario(self,nombre_cuenta):
         print(f'User: {self.name}, Email: {self.email}')
-        self.cuenta.mostrar_info_cuenta()
+        self.cuenta[nombre_cuenta].mostrar_info_cuenta()
         return self
     
-    def generar_interes(self):
-        self.cuenta.generar_interés()
+    def generar_interes(self,nombre_cuenta):
+        self.cuenta[nombre_cuenta].generar_interés()
+        print(f'Generando interés')
         return self
 
 richard = Usuario('Richard Torres','rtorresbarriga@gmail.com')
-tote = Usuario('tote', 'tote@gmail.com')
+richard.crear_cuenta('bank1',0.2)
+richard.hacer_deposito('bank1',1000)
+richard.retiro('bank1',200)
+richard.crear_cuenta('bank2',0.5)
+richard.hacer_deposito('bank2',2000)
 
-richard.hacer_deposito(500)
-richard.transferencia(tote,100)
-richard.mostrar_balance_usuario()
-tote.mostrar_balance_usuario()
+
+richard.transferencia_interna('bank1','bank2',200)
+richard.mostrar_balance_usuario('bank1')
+richard.mostrar_balance_usuario('bank2')
+
 
 
 
